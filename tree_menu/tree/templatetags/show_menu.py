d17645e23bd040cb00aafa0b6a9a0ca3ec88e5bf
Den2605 <rezerv50@yandex.ru>
2023-11-06 17:59:33 +0300

    fix

--- tree_menu/tree/templatetags/show_menu.py
+++ tree_menu/tree/templatetags/show_menu.py
@@ -11,11 +11,11 @@ register = template.Library()
 def draw_menu(context, slug_menu):
     tag_name = context["slug_menu"]
     # имя тега совпадает с названием таблицы
+    html = ""
     if tag_name == slug_menu:
         menu = get_object_or_404(Menu, slug=slug_menu)
         # получаем текущую корневую директорию
         root_items = Item.objects.filter(parent__isnull=True, menu=menu.id)
-        html = ""
         for root_item in root_items:
             html += _get_menu(root_item, html)
 
@@ -25,47 +25,15 @@ def draw_menu(context, slug_menu):
     # получаем значение текущего подпункта
     current_items = get_object_or_404(Item, slug=tag_name)
 
-    # проверяем сколько есть вложенных элементов
-    # если их менее двух, значит отображается вся таблица
-    child_current_items = Item.objects.filter(parent=current_items.id)
-    if child_current_items.exists():
-        next_child_current_items = Item.objects.filter(
-            parent=child_current_items[0].id
-        )
-        if next_child_current_items.exists():
-            stop_item = next_child_current_items[0].name
-        else:
-            stop_item = None
-    else:
-        stop_item = None
-
-    # получаем id меню
+    # получаем меню
     menu = current_items.menu
 
     # получаем корневые папки данного меню
     root_items = Item.objects.filter(parent__isnull=True, menu=menu)
-    html = ""
     for root_item in root_items:
-        if stop_item:
-            html += _get_item(root_item, html, stop_item=stop_item)
-        else:
-            html += _get_item(root_item, html)
+        stop_item = _get_stop(current_items)
+        html += _get_menu(root_item, html, stop_item=stop_item)
     return mark_safe(html)
-
-
-def _get_item(menu, html, stop_item=None):
-    html = "<ul>"
-    html += "<li>"
-    html += f'<a href="{menu.get_absolute_url()}">{menu.name}</a>'
-    html += "</li>"
-    children_items = menu.childrens.all()
-    if children_items.exists():
-        for sub_item_children in children_items:
-            if sub_item_children.name == stop_item:
-                return html
-            html += _get_menu(sub_item_children, html, stop_item=stop_item)
-    html += "</ul>"
-    return html
 
 
 def _get_menu(menu, html, stop_item=None):
@@ -81,3 +49,16 @@ def _get_menu(menu, html, stop_item=None):
             html += _get_menu(sub_item_children, html, stop_item=stop_item)
     html += "</ul>"
     return html
+
+
+def _get_stop(current_items):
+    # проверяем сколько есть вложенных элементов
+    # если их менее двух, значит отображается вся таблица
+    child_current_items = Item.objects.filter(parent=current_items.id)
+    if child_current_items.exists():
+        next_child_current_items = Item.objects.filter(
+            parent=child_current_items[0].id
+        )
+        if next_child_current_items.exists():
+            return next_child_current_items[0].name
+    return None
