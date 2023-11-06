8d00e8d1fdb33ea3c324b84a556a5172c1076113
Den2605 <rezerv50@yandex.ru>
2023-11-06 19:36:04 +0300

    Add docstring

--- tree_menu/tree/views.py
+++ tree_menu/tree/views.py
@@ -2,11 +2,15 @@ from django.views.generic import TemplateView
 
 
 class TreeView(TemplateView):
+    """
+    Функция для отображения отображения меню.
+    """
+
     template_name = "tree/index.html"
 
     def get_context_data(self, **kwargs):
+        """Формирует контекст, предаёт данные из запроса пользователя."""
         context = super().get_context_data(**kwargs)
-        # Получить slug меню из URL
         slug_menu = self.kwargs["slug"]
         context["slug_menu"] = slug_menu
         return context
