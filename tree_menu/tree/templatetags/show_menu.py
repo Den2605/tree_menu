eb75aca87c72062b39aef319cb8d1bfb908e9a3d
Den2605 <rezerv50@yandex.ru>
2023-11-06 19:22:18 +0300

    Fix recursion error

--- tree_menu/tree/templatetags/show_menu.py
+++ tree_menu/tree/templatetags/show_menu.py
@@ -10,55 +10,47 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def draw_menu(context, slug_menu):
     tag_name = context["slug_menu"]
-    # имя тега совпадает с названием таблицы
-    html = ""
+    menu = get_object_or_404(Menu, slug=slug_menu)
+
     if tag_name == slug_menu:
-        menu = get_object_or_404(Menu, slug=slug_menu)
-        # получаем текущую корневую директорию
-        root_items = Item.objects.filter(parent__isnull=True, menu=menu.id)
-        for root_item in root_items:
-            html += _get_menu(root_item, html)
+        root_items = Item.objects.filter(parent__isnull=True, menu=menu)
+    else:
+        current_items = get_object_or_404(Item, slug=tag_name)
+        root_items = Item.objects.filter(
+            parent__isnull=True, menu=current_items.menu
+        )
 
-        return mark_safe(html)
-
-    # имя тега не совпадает с названием таблицы
-    # получаем значение текущего подпункта
-    current_items = get_object_or_404(Item, slug=tag_name)
-
-    # получаем меню
-    menu = current_items.menu
-
-    # получаем корневые папки данного меню
-    root_items = Item.objects.filter(parent__isnull=True, menu=menu)
+    html = ""
     for root_item in root_items:
-        stop_item = _get_stop(current_items)
-        html += _get_menu(root_item, html, stop_item=stop_item)
+        if tag_name == slug_menu:
+            stop_item = None
+        else:
+            stop_item = _get_stop(current_items)
+        html += _get_menu(root_item, stop_item)
+
     return mark_safe(html)
 
 
-def _get_menu(menu, html, stop_item=None):
+def _get_menu(menu, stop_item=None):
     html = "<ul>"
     html += "<li>"
     html += f'<a href="{menu.get_absolute_url()}">{menu.name}</a>'
     html += "</li>"
     children_items = menu.childrens.all()
-    if children_items.exists():
-        for sub_item_children in children_items:
-            if sub_item_children.name == stop_item:
-                return html
-            html += _get_menu(sub_item_children, html, stop_item=stop_item)
+    for sub_item_children in children_items:
+        if sub_item_children.name == stop_item:
+            break
+        html += _get_menu(sub_item_children, stop_item)
     html += "</ul>"
     return html
 
 
 def _get_stop(current_items):
-    # проверяем сколько есть вложенных элементов
-    # если их менее двух, значит отображается вся таблица
-    child_current_items = Item.objects.filter(parent=current_items.id)
-    if child_current_items.exists():
+    child_current_items = Item.objects.filter(parent=current_items)
+    if child_current_items:
         next_child_current_items = Item.objects.filter(
-            parent=child_current_items[0].id
+            parent=child_current_items[0]
         )
-        if next_child_current_items.exists():
+        if next_child_current_items:
             return next_child_current_items[0].name
     return None
